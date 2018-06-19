8b05a30af7f89c4336d16737ea303db762323c25
karuhanga <karuhanga475@gmail.com>
2018-06-19 10:39:58 +0300

    Modified models for coherence

--- core/models.py
+++ core/models.py
@@ -13,11 +13,19 @@ class Section(models.Model):
     description = models.TextField()
 
 
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    contact_person = models.CharField(max_length=30)
+    email = models.EmailField()
+    contact = models.CharField(max_length=15)
+
+
 class Contract(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    section = models.ForeignKey('Section', on_delete=models.DO_NOTHING)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
     start_date = models.DateField()
     expiry_date = models.DateField()
     contract_manager = models.ForeignKey('ContractManager', on_delete=models.DO_NOTHING)
-    section = models.ForeignKey('Section', on_delete=models.DO_NOTHING)
 
